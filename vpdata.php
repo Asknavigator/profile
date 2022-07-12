743f328ec367069f7254a0c22d62c96b64b053c1
digiheadway <99395515+digiheadway@users.noreply.github.com>
2022-07-12 21:52:41 +0530

    column change

--- vpdata.php
+++ vpdata.php
@@ -30,7 +30,7 @@ if($res["res"] != 'success' or $vendor == null ){
 }
 
 // use $vendor['column name'];
-$cfirstnamedb = $vendor['company name'];
+$cfirstnamedb = $vendor['company'];
 $cfirstnamedb = strtolower($cfirstnamedb);
 
 echo $cfirstnamedb;
