d268bd58d5a3dc4e201d74fd52fb9d98a962a1f4
digiheadway <99395515+digiheadway@users.noreply.github.com>
2022-07-12 11:18:32 +0530

    data Added

---
+++ data.php
@@ -0,0 +1,18 @@
+<?php
+
+$url = "https://api.asknavigator.com/?o=vendors_view&a=read&i=456";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
+
+?>
+
