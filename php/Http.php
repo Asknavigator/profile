e8341878f8492c3e3c6b64426472c4e07d2626a1
Rahul Mittal <78584898+rahulmitta1@users.noreply.github.com>
2022-07-22 14:10:55 +0530

    Updated

---
+++ php/Http.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Http
+{
+
+    static function get($url)
+    {
+        return file_get_contents($url);
+    }
+
+    static function get_curl($url)
+    {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        //for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $resp = curl_exec($curl);
+        curl_close($curl);
+        return $resp;
+    }
+
+}
+
+
+?>
--- vpdata.php
+++ vpdata.php
@@ -9,20 +9,10 @@ echo "Invalid Url";
 die;
 }
 
-$url = "https://api.asknavigator.com/?o=vendors_view&a=read&i=".$vid;
 
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-//for debug only!
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-$resp = curl_exec($curl);
-curl_close($curl);
-// var_dump($resp);
-
+include_once("php/Http.php");
+$resp = Http->get($url);
+echo $resp;
 $res = json_decode($resp, true);
 $vendor = $res['data'] ?? null; 
 if($res["res"] != 'success' or $vendor == null ){
