9f70b1a1281a875aacb24b81213b2d4cb56a8293
Rahul Mittal <78584898+rahulmitta1@users.noreply.github.com>
2022-07-22 14:51:35 +0530

    Updated

--- vpdata.php
+++ vpdata.php
@@ -1,33 +1,55 @@
 <?php
 
-$url= $_SERVER['REQUEST_URI'];    
-$vid = explode("-", explode("/", $url)[1])[1];
-$cfirstname = explode("-", explode("/", $url)[1])[0];
 
-If(!$vid OR !$cfirstname){
-echo "Invalid Url";
-die;
+function parse_nameid(&$ven_id, &$first_name)
+{
+  $url = $_SERVER['REQUEST_URI'];
+
+  $ven_id = explode("-", explode("/", $url)[1])[1];
+  $first_name = explode("-", explode("/", $url)[1])[0];
+
+  if (!$ven_id or !$first_name) {
+    echo "Invalid Url";
+    die;
+  }
 }
 
+function main()
+{
+  include_once("php/Http.php");
+  $ven_id = null;
+  $first_name = null;
+  parse_nameid($ven_id, $first_name);
 
-include_once("php/Http.php");
-$resp = Http->get($url);
-echo $resp;
-$res = json_decode($resp, true);
-$vendor = $res['data'] ?? null; 
-if($res["res"] != 'success' or $vendor == null ){
+  $url = "https://api.asknavigator.com/?o=vendors_view&a=read&i=$ven_id";
+  $resp = Http->get($url);
+
+  echo $resp;
+  $res = json_decode($resp, true);
+  $vendor = $res['data'] ?? null;
+
+  if ($res["res"] != 'success' or $vendor == null) {
   // die('Wrong Id');
+  }
+
+  // use $vendor['column name'];
+  $cfirstnamedb = $vendor['company'];
+  $cfirstnamedb = explode(" ", $cfirstnamedb)[0];
+  $cfirstnamedb = strtolower($cfirstnamedb);
+
+  if ($cfirstnamedb != $cfirstname) {
+  // echo "Invalid Url";
+  // die;
+  // do nothing for now
+  }
+
 }
 
-// use $vendor['column name'];
-$cfirstnamedb = $vendor['company'];
-$cfirstnamedb = explode(" ", $cfirstnamedb)[0];
-$cfirstnamedb = strtolower($cfirstnamedb);
-
-if ($cfirstnamedb != $cfirstname){
-   // echo "Invalid Url";
-   // die;
-   // do nothing for now
+try {
+  main()
+} catch (\Throwable $th) {
+  echo $th->getMessage();
 }
+
 ?>
 
